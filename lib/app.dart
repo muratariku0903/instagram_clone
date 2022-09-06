ad1f985e0b38ffc796012e8e24f8a8ccb55a5a7f
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-09-07 02:50:22 +0900

    update main and app dart file

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:instagram/di_container.dart';
+import 'package:instagram/domain/user/models/user.dart';
+import 'package:instagram/pages/app/app_notifier.dart';
+import 'package:instagram/pages/app/user_notifier.dart';
+import 'package:instagram/pages/app/states/user_state.dart';
+import 'package:instagram/pages/signin/signin_page.dart';
+import 'package:instagram/pages/home/home_page.dart';
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      
-    ]);
+    return MultiProvider(
+      providers: [
+        ...domainProviders,
+        ...notifierProviders,
+      ],
+      child: const _MaterialApp(),
+    );
+  }
+}
+
+class _MaterialApp extends StatelessWidget {
+  const _MaterialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      navigatorKey: context.watch<AppNotifier>().navigatorKey,
+      debugShowCheckedModeBanner: false,
+      home: const ConfigPage(),
+    );
+  }
+}
+
+class ConfigPage extends StatefulWidget {
+  const ConfigPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConfigPage> createState() => _ConfigPageState();
+}
+
+class _ConfigPageState extends State<ConfigPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      judgeAuthStatus();
+    });
+  }
+
+  void judgeAuthStatus() async {
+    final userNotifier = context.read<UserNotifier>();
+    userNotifier.listenAuthStatus();
+    final userState = context.read<UserState>();
+    if (userState.userStatus == UserStatus.email) {
+      await userNotifier.fetchUser();
+      print(context.read<UserState>().user);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userStatus = context.select((UserState value) => value).userStatus;
+
+    return (userStatus == UserStatus.email)
+        ? HomePage.wrapped()
+        : SignInPage.wrapped();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:instagram/firebase_options.dart';
-import 'package:instagram/pages/signin/signin_page.dart';
+import 'package:instagram/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, [bool mounted = true]) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: mounted ? SignInPage.wrapped() : const Text('No mounted.'),
-    );
-  }
+  runApp(const App());
 }
