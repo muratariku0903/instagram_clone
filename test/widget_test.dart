de874cb4ab9c9aba838eb22568e0211028ccff3a
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-09-11 05:22:17 +0900

    add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:instagram/app.dart';
+import 'package:instagram/domain/post/models/comment.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+  test('user test', () {
+    Timestamp now = Timestamp.now();
+    Comment comment1 = Comment.fromJson({
+      'comment': 'hello',
+      'uid': '1234',
+      'dateOfComment': now,
+      'isLinked': false,
+    });
+    Comment comment2 = Comment.fromJson({
+      'comment': 'hello',
+      'uid': '1234',
+      'dateOfComment': now,
+      'isLinked': false,
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(comment1, comment2);
   });
+}
+
+class User {
+  String name;
+  int age;
+  User(this.name, this.age);
+
+  static bool isSameUser(User user1, User user2) {
+    if (user1.name != user2.name) return false;
+    if (user1.age != user2.age) return false;
+    return true;
+  }
 }
